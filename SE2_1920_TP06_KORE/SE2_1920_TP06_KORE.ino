ce2a00b493c97a2ce9f4c59a09741ad10b396ade
Racnavor <KYE0838@hotmail.com>
2019-11-08 12:51:59 +0100

    Update SE2_1920_TP06_KORE.ino
    Update du TP06

--- SE2_1920_TP06_KORE/SE2_1920_TP06_KORE.ino
+++ SE2_1920_TP06_KORE/SE2_1920_TP06_KORE.ino
@@ -5,15 +5,10 @@
 #include <Keypad.h>
 
 #define BTN_ON analogRead(A1)
-#define LED1 22
-#define LED2 23
-#define LED3 24
-#define LED4 25
-#define LED5 26
-#define LED6 27
 #define LED_INFO 28
 
-int potar, etat_led, etat_avant;
+int LED[] = {22, 23, 24, 25, 26, 27};
+int potar, but_led, etat_led = 0;
 long tempo = 0;
 boolean ledInfo = true;
 
@@ -24,13 +19,12 @@ void setup()
   //Initialisation des différentes pins
   pinMode(18, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(18), au, CHANGE);
-  Serial.begin(9600);
-  pinMode(LED1, OUTPUT);
-  pinMode(LED2, OUTPUT);
-  pinMode(LED3, OUTPUT);
-  pinMode(LED4, OUTPUT);
-  pinMode(LED5, OUTPUT);
-  pinMode(LED6, OUTPUT);
+
+  for (int i = 0; i < sizeof(LED); i++)
+  {
+    pinMode(LED[i], OUTPUT);
+  }
+
   pinMode(LED_INFO, OUTPUT);
   ledOff();
   digitalWrite(LED_INFO, HIGH);
@@ -45,104 +39,40 @@ void setup()
 void loop()
 {
   potar = map(analogRead(A0), 0, 1023, 0, 1000); // Redéfinition de la plage de valeur
-  Serial.println(potar / 10); // Affichage de la valeur du potentiomètre en pourcent sur l'écran
   MFS.write(potar / 10); // Affichage de la valeur du potentiomètre en pourcent sur l'afficheur 7 segments
   etat_led = map(analogRead(A0), 0, 1023, 0, 6);
-  Serial.println(etat_led);
 
   if (!BTN_ON)
   {
     while (!BTN_ON) {}
     digitalWrite(LED_INFO, HIGH);
     ledInfo = false;
-    switch (etat_led)
+
+    if (etat_led < but_led)
     {
-      case 1:
-        ledOff();
+      for (int i = etat_led; i < but_led; i++)
+      {
         delay(1000);
-        digitalWrite(LED1, !digitalRead(LED1));
-        ledInfo = true;
-        etat_avant = 1;
-        break;
-
-      case 2:
-        ledOff();
-        delay(1000);
-        digitalWrite(LED1, !digitalRead(LED1));
-        delay(1000);
-        digitalWrite(LED2, !digitalRead(LED2));
-        ledInfo = true;
-        etat_avant = 2;
-        break;
-
-      case 3:
-        ledOff();
-        delay(1000);
-        digitalWrite(LED1, !digitalRead(LED1));
-        delay(1000);
-        digitalWrite(LED2, !digitalRead(LED2));
-        delay(1000);
-        digitalWrite(LED3, !digitalRead(LED3));
-        ledInfo = true;
-        etat_avant = 3;
-        break;
-
-      case 4:
-        ledOff();
-        delay(1000);
-        digitalWrite(LED1, !digitalRead(LED1));
-        delay(1000);
-        digitalWrite(LED2, !digitalRead(LED2));
-        delay(1000);
-        digitalWrite(LED3, !digitalRead(LED3));
-        delay(1000);
-        digitalWrite(LED4, !digitalRead(LED4));
-        ledInfo = true;
-        etat_avant = 4;
-        break;
-
-      case 5:
-        ledOff();
-        delay(1000);
-        digitalWrite(LED1, !digitalRead(LED1));
-        delay(1000);
-        digitalWrite(LED2, !digitalRead(LED2));
-        delay(1000);
-        digitalWrite(LED3, !digitalRead(LED3));
-        delay(1000);
-        digitalWrite(LED4, !digitalRead(LED4));
-        delay(1000);
-        digitalWrite(LED5, !digitalRead(LED5));
-        ledInfo = true;
-        etat_avant = 5;
-        break;
-
-      case 6:
-        ledOff();
-        delay(1000);
-        digitalWrite(LED1, !digitalRead(LED1));
-        delay(1000);
-        digitalWrite(LED2, !digitalRead(LED2));
-        delay(1000);
-        digitalWrite(LED3, !digitalRead(LED3));
-        delay(1000);
-        digitalWrite(LED4, !digitalRead(LED4));
-        delay(1000);
-        digitalWrite(LED5, !digitalRead(LED5));
-        delay(1000);
-        digitalWrite(LED6, !digitalRead(LED6));
-        ledInfo = true;
-        etat_avant = 6;
-        break;
-
-      default:
-        break;
+        Serial.print("Position : ");
+        Serial.println(etat_led);
+        digitalWrite(LED[i], LOW);
+        etat_led++;
+      }
     }
-    if(etat_avant > etat_led)
+    else if (etat_led > but_led)
     {
-      
+      for (int i = etat_led; i > but_led - 1; i--)
+      {
+        delay(1000);
+        Serial.print("Position : ");
+        Serial.println(etat_led);
+        digitalWrite(LED[i], HIGH);
+        etat_led--;
+      }
     }
+    ledInfo = true;
   }
+
   if (ledInfo)
   {
     if (millis() > tempo + 500)
@@ -151,17 +81,14 @@ void loop()
       tempo = millis();
     }
   }
-
 }
 
 void ledOff()
 {
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, HIGH);
-  digitalWrite(LED4, HIGH);
-  digitalWrite(LED5, HIGH);
-  digitalWrite(LED6, HIGH);
+  for (int i = 0; i < sizeof(LED); i++)
+  {
+    digitalWrite(LED[i], HIGH);
+  }
 }
 
 void au()
@@ -179,10 +106,15 @@ void au()
     digitalWrite(LED_INFO, !digitalRead(LED_INFO));
     delay(500);
     }*/
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, HIGH);
-  digitalWrite(LED4, HIGH);
-  digitalWrite(LED5, HIGH);
-  digitalWrite(LED6, HIGH);
+  for (int i = 0; i < sizeof(LED); i++)
+  {
+    digitalWrite(LED[i], HIGH);
+  }
+  
+//  digitalWrite(LED1, HIGH);
+//  digitalWrite(LED2, HIGH);
+//  digitalWrite(LED3, HIGH);
+//  digitalWrite(LED4, HIGH);
+//  digitalWrite(LED5, HIGH);
+//  digitalWrite(LED6, HIGH);
 }
